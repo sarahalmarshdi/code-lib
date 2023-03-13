5d735fff522b82262599e37537ade1e1ea31094d
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:45:08 +0300

    Add files via upload

---
+++ dd.java
@@ -0,0 +1,18 @@
+public class dd{
+public static void  main(String [] args){
+    String out ;
+    int b,a ;
+    b=2;
+    a=4;
+    out=a==b?"yes":"No";
+    System.out.println("Ans:"+ out);
+    
+
+
+
+
+
+
+
+}
+}
