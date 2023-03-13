543f0f08fa85750ca2f13292556b02cbd4e498f6
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:44:26 +0300

    Add files via upload

---
+++ kk.java
@@ -0,0 +1,12 @@
+public class kk{
+public static void main(String[]args){
+    int x=3;
+    
+    do {
+        System.out.println(x);
+        ++x;
+    
+    }while (x<=10);
+    System.out.println();
+    
+}}
