1f9d9beccfbc6b85f0cfea4fb087dbf9a4cc716c
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:46:53 +0300

    Add files via upload

---
+++ days_in_week.java
@@ -0,0 +1,16 @@
+public class days_in_week{
+public static void main(String[]args){
+    int weeks=3;
+    int days = 7;
+    for (int i=1 ; i<=weeks ; i++){
+        System.out.println("week:" + i );
+        for (int j=1 ; j<=days ; j++ ){
+        System.out.println("Days : "+ j); 
+        }
+        
+    }
+}
+
+
+
+}
