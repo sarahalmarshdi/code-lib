acbfc370213fea892419f213b793e41450502acc
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 16:11:54 +0300

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,67 +1,52 @@
+/**
+ * A class representing students for a simple BlueJ demo program.
+ *
+ * @author  Michael Kölling
+ * @version 1.0, January 1999
+ */
 
-
-public class Student  
+class Student extends Person
 {
-    private String firstName;
-    private String lastName; 
-    private double GPA ;
+    private String SID;    // student ID number
 
-    public Student(){
-        firstName = "" ;
-        lastName = "";
-        GPA = 0;
-        
+    /**
+     * Create a student with default settings for detail information.
+     */
+
+    Student()
+    {
+        super("(unknown name)", 0000);
+        SID = "(unknown ID)";
     }
-    
-    public Student(String firstName , String lastName ){
-       
-        this.firstName= firstName;
-        this.lastName = lastName; 
-        
+ 
+
+    /**
+     * Create a student with given name, year of birth and student ID
+     */
+    Student(String name, int yearOfBirth, String studentID)
+    {
+        super(name, yearOfBirth);
+        SID = studentID;
     }
-    
-    public void setFirstName(String firstName ){
-         this.firstName = firstName;
-         
-        
+
+    /**
+     * Return the student ID of this student.
+     */
+    public String getStudentID()
+    {
+       return SID;
     }
-    public void setLastName(String LastName ){
-        this.lastName = lastName; }
-        
-  public String getName(){
-      return "My Full name is " + firstName +" "+ lastName;
-      
-      
-  }
-  public String getfirstName(){ 
-      return firstName ;
-  }
-      public String getLasName(){
-          return lastName;
-      }
-      public void setGPA(double GPA ){
-          this.GPA = GPA ;
-          
-          
-      }
-      public double getGPA (){
-          return GPA ;
-          
-      }
-    
+
+    /**
+     * Return a string representation of this object.
+     */
+    public String toString()    // redefined from "Person"
+    {
+        return super.toString() +
+               "Student\n" +
+               "Student ID: " + SID + "\n";
+    }
+
 }
 
- class main
-{ public static void main (String[] args)
-    
 
-    { Student student1 = new Student ("Sarah" , "Matar");
-        String myFullName = student1.getName();
-        System.out.println(myFullName);
-        student1.setGPA(3.5);
-        System.out.println(student1.getGPA());
-    }}
-
-        
-        
-        
