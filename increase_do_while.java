ac710c1211bd756291a233fa85702086f19c9292
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:43:18 +0300

    Add files via upload

---
+++ increase_do_while.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class increase_do_while{
+public static void main(String[]args){
+    int x=10;
+Scanner in = new Scanner(System.in);
+do{
+System.out.println("Enter the  number");
+ x = in.nextInt();}
+while (x<=10);
+System.out.println();
+
+
+
+
+
+
+}
+
+}
