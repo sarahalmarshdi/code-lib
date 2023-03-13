88d3c98f2569f6eeac55d95f75e4d78269c53a7c
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:39:00 +0300

    Add files via upload

---
+++ Student_grade.java
@@ -0,0 +1,33 @@
+ import java.util.Scanner;
+ public class Student_grade{ 
+     
+     public static void main(String[] args){
+         System.out.println("Please Entere the grade");
+         char  grade = 'C';
+         switch(grade){
+             case 'A': System.out.println("Exlcellent");
+             break;
+             
+             case 'B' : System.out.println(" well done");
+              break;
+             case 'C': System.out.println("you passed ");
+             break ;
+             case 'D' : System.out.println("d");
+            break ;
+             default: System.out.println("Not in A,B,C and D");}
+             System.out.println( grade);
+         }
+         
+        
+        
+        
+        
+        }
+     
+     
+    
+    
+    
+    
+    
+    
