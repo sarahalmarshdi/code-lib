91990e9610f194cbb3de6ed6d207786695239609
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:39:37 +0300

    Add files via upload

---
+++ Large_Number.java
@@ -0,0 +1,12 @@
+public class Large_Number{
+public static void main(String[]args){
+int x= 9;
+int y = 10;
+int z= 3;
+int largestNumber=(x>y)?(x>y?x:y):(y>x?y:x);
+System.out.println("The largesNumber"+ largestNumber);
+}
+
+
+
+}
