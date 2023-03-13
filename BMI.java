71152103a0f491955e46ad11814a30f5ec408abc
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 16:02:44 +0300

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class BMI{
+    public static  double calculateBMUI(double weight ,double height){
+    double  BMI ;
+    BMI = weight/(height * height );
+    return BMI ;
+}
+
+}
