a2d74bc72a96d0b2e8d401e4cfc5faaa8dde0d79
sarahalmarshdi <123188609+sarahalmarshdi@users.noreply.github.com>
2023-03-13 15:48:46 +0300

    Add files via upload

---
+++ count.java
@@ -0,0 +1,16 @@
+
+public class count{
+public static void main(String[]args){
+int x;
+for ( x= 1 ; x<= 10 ; x++)
+{if (x==6)
+    continue ;
+
+System.out.println(x);
+
+}System.out.printf("%nBroke out of loop at count = %d%n" ,x );
+
+
+
+
+}}
